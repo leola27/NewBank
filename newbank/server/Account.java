0764b0bd10e8f57b7e6f1cd279e222d1ad8a0a3e
KamWright <108580184+KamWright@users.noreply.github.com>
2022-07-10 10:22:17 +0100

    Update Account.java
    Attempt to transfer amounts from one account to another whist checking the amount is valid. I think the methods are best placed in this file, but wasn't too sure, so please feel free to advise/change

--- newbank/server/Account.java
+++ newbank/server/Account.java
@@ -17,5 +17,39 @@ public class Account {
 	public String toString() {
 		return (accountName + ": " + openingBalance);
 	}
+	
+	// may be worth changing openingBalance too just balance
+
+
+	public double getOpeningBalance(){
+	return openingBalance ;
+	}
+
+	public void setBalance(double newBalance) {
+   	this.openingBalance = newBalance;
+	}
+
+	private void checkPositiveAmount(double amount) {
+  	if (amount.asBigDecimal().compareTo(BigDecimal.ZERO) < 0) {
+   	throw new NegativeAmountException("Unable to process negative amount: " + amount);
+	 }
+	}
+
+	private void checkSufficientFunds(double amount) {
+ 	if (openingBalance.compareTo(amount) < 0) {
+   	throw new InsufficientBalanceException("Not enough funds to withdraw: " + amount);
+	 }
+	}
+
+	public void transferMoney(Account thisAccount, Account toAccount, double amount){
+    	if(thisAccount.getOpeningbalance() > 0) {
+        toAccount.setBalance(toAccount.openingBalance += amount);
+        thisAccount.setBalance(this.openingBalance -= amount);
+   	} else {
+        System.out.println("Transfer Failed, not enough funds to complete transcation");
+    	 }
+	}
+
+// I feel like this method can be used to transfer to person to person or adapted too
 
 }
