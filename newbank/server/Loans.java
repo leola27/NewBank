93a7e71f38af40f2b25b6aa7cd7bf34fc7ff1a7a
JCAHolland <jonathan.holland1314>
2022-07-12 01:31:46 +0100

    Complete refactor and layout change. Please let me know if there are any changes I need to make before others in the group start working!

---
+++ newbank/server/Loans.java
@@ -0,0 +1,19 @@
+package newbank.server;
+
+import newbank.server.Customer.Customer;
+
+public class Loans {
+
+    public Loans() {
+    }
+
+    public String offerLoan(double amountRequested, Customer customer) {
+        double maxAmount = customer.getAccount("Main").getBalance() * 3;
+        if (amountRequested >= maxAmount) {
+            return "We can offer you up to £" + maxAmount;
+        }
+        System.out.println("adding balance");
+        customer.getAccount("Main").addBalance(amountRequested);
+        return amountRequested + " added to your account";
+    }
+}
