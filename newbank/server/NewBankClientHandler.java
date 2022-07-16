19a63f7fc9b1b22efa56c44fd4c50640dec7e568
joaomarcelofm-gyg <joao.martins@getyourguide.com>
2022-07-16 17:21:20 +0200

    Include logout feature

--- newbank/server/NewBankClientHandler.java
+++ newbank/server/NewBankClientHandler.java
@@ -41,12 +41,18 @@ public class NewBankClientHandler extends Thread{
 					CustomerID customer = bank.checkLogInDetails(userName, password);
 					// if the user is authenticated then get requests from the user and process them
 					if(customer != null) {
-						out.println("Log In Successful. What do you want to do?");
-						while(true) {
+						out.println("Log In Successful.");
+						while(true && customer != null) {
+              out.println("What do you want to do?");
 							String request = in.readLine();
-							System.out.println("Request from " + customer.getKey());
-							String response = bank.processRequest(customer, request);
-							out.println(response);
+
+              if (request.equals("LOGOUT")) {
+                customer = null;
+              } else {
+                System.out.println("Request from " + customer.getKey());
+                String response = bank.processRequest(customer, request);
+                out.println(response);
+              }
 						}
 					}
 					else{
