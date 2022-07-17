e0dfb68e15c7c53cb7bb29318d49de67d3ebf552
KamWright <108580184+KamWright@users.noreply.github.com>
2022-07-17 20:52:25 +0100

    Method to get monthly repayment amount
    I have added this method into this class, but think elements may conflict with the current version

--- newbank/server/Loans.java
+++ newbank/server/Loans.java
@@ -1,13 +1,19 @@
 package newbank.server;
 
 import newbank.server.Customer.Customer;
+import newbank.server.Account;
+import java.lang.Math;
 
 public class Loans {
     private double loanAmount;
     private double loanPaid;
-    public Loans(double loanAmount) {
+    public Loans(double balance, double monthlyInterestRate, int numberOfYears,
+      double loanAmount) {
         this.loanAmount = loanAmount;
         this.loanPaid = 0;
+        this.balance = getBalance();
+        this.monthlylInterestRate = monthlyInterestRate;
+        this.numberOfYears = numberOfYears;
     }
 
     public double getLoanAmount(){
@@ -20,6 +26,13 @@ public class Loans {
 
     public void repay(double amount){
         loanPaid += amount;
+    }
+    
+    public double getMonthlyLoanRepayment() {
+        double monthlyInterestRate = 0.1;
+        double monthlyPayment = getAccount("Main").getBalance() * monthlyInterestRate / (1 -
+        (1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12))); //calculate monthly repayment
+        return monthlyPayment;    
     }
 
     public void addLoan(double amount){
