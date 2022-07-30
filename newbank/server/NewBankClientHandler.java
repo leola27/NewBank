e8504691871cc546d556aaf226ef265c2c39c7c2
Carl Harrison <carlharrison1@live.co.uk>
2022-07-30 16:36:17 +0100

    Added 3 password attempt functionality
    After 3 failed password attempts, the client is blocked for 30 seconds. Of course, this is a non-suitable solution in the real world but I can iterate on this to perhaps terminate the client thread(?)

--- newbank/server/NewBankClientHandler.java
+++ newbank/server/NewBankClientHandler.java
@@ -8,12 +8,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Set;
 
-public class NewBankClientHandler extends Thread{
+public class NewBankClientHandler extends Thread {
 
 	private NewBank bank;
 	private BufferedReader in;
 	private PrintWriter out;
+	int loginAttempts = 3;
 
 	public NewBankClientHandler(Socket s) throws IOException {
 		bank = NewBank.getBank();
@@ -25,22 +27,29 @@ public class NewBankClientHandler extends Thread{
 		// keep getting requests from the client and processing them
 		try {
 
-			while (true){
+			while (true) {
 				// ask for user name
 				out.println("Enter Username");
 				String userName = in.readLine();
-				userName = userName.toLowerCase().replaceAll(" ","");
+				userName = userName.toLowerCase().replaceAll(" ", "");
 				// ask for password
 				out.println("Enter Password");
 				String password = in.readLine();
 				out.println("Checking Details...");
-				// authenticate user and get customer ID token from bank for use in subsequent requests
-				if(bank.isCustomer(userName)){
+				// authenticate user and get customer ID token from bank for use in subsequent
+				// requests
+				if (bank.isCustomer(userName)) {
+					// List all running threads (performing this as I'm thinking of terminating
+					// client thread after failing login 3 times)
+					Set<Thread> threads = Thread.getAllStackTraces().keySet();
+					for (Thread t : threads) {
+						System.out.println(t.getName());
+					}
 					CustomerID customer = bank.checkLogInDetails(userName, password);
 					// if the user is authenticated then get requests from the user and process them
-					if(customer != null) {
+					if (customer != null) {
 						out.println("Log In Successful.");
-						while(customer != null) {
+						while (customer != null) {
 							out.println("What do you want to do?");
 							String request = in.readLine();
 
@@ -52,30 +61,36 @@ public class NewBankClientHandler extends Thread{
 								out.println(response);
 							}
 						}
+					} else {
+						out.println("Incorrect password. Test Try again");
+						loginAttempts--;
+						out.println("You have " + loginAttempts + " attempts remaining");
+						if (loginAttempts == 0) {
+							out.println(
+									"You've entered an incorrect password 3 times, you are now locked out for 30 seconds");
+							loginAttempts = 3;
+							Thread.sleep(30000);
+						}
 					}
-					else{
-						out.println("Incorrect password. Try again");
-					}
-				}
-				else {
+
+				} else {
 					out.println("Username doesn't exist. Would you like to create an account with that name? (y/n)");
 					String request = in.readLine();
 					if (request.toLowerCase().equals("y")) {
 						out.println("Please enter the account name");
 						String accountName = in.readLine();
 						Customer newCustomer = new Customer(userName, password, accountName, 0);
-						newCustomer.addNewCustomerToDb(userName,password);
-//						bank.newCustomer(userName, password, accountName, 0);
+						newCustomer.addNewCustomerToDb(userName, password);
+						// bank.newCustomer(userName, password, accountName, 0);
 						out.println("Success, please login with your new account");
 					} else {
 						out.println("Please try again");
 					}
 				}
 			}
-		} catch (IOException e) {
+		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			try {
 				in.close();
 				out.close();
